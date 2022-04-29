8f8950b97dca92962a798eaa135e9709213b419d
Cameron Zach <czach@sevone.com>
2022-04-29 19:48:04 -0400

    initial commit

---
+++ save_google.py
@@ -0,0 +1,135 @@
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from random import randrange
+
+from util import coordsToA1
+
+
+def google(creds, name, image, questions, answers):
+    creds = Credentials.from_authorized_user_info(creds)
+    service = build('sheets', 'v4', credentials=creds)
+
+    spreadsheet = {
+        'properties': {
+            'title': name
+        }
+    }
+
+    spreadsheet = service.spreadsheets() \
+        .create(body=spreadsheet, fields='spreadsheetId') \
+        .execute()
+
+        
+
+    values = [['Questions', 'Answers'], *([question] for question in questions)]
+    body = {
+        'values': values
+    }
+    result = service.spreadsheets().values().update(
+        spreadsheetId=spreadsheet['spreadsheetId'], range='A:B',
+        valueInputOption='USER_ENTERED', body=body).execute()
+    print('{0} cells updated.'.format(result.get('updatedCells')))
+
+    colors = image.getdata()
+    requests = [
+    {
+      "deleteDimension": {
+        "range": {
+          "sheetId": 0,
+          "dimension": "ROWS",
+          "startIndex": len(questions) + 1,
+        }
+      }
+    },
+    {
+      "deleteDimension": {
+        "range": {
+          "sheetId": 0,
+          "dimension": "COLUMNS",
+          "startIndex": 3,
+        }
+      }
+    },
+    {
+      "appendDimension": {
+        "sheetId": 0,
+        "dimension": "ROWS",
+        "length": image.height - len(questions) - 1
+      }
+    },
+    {
+      "appendDimension": {
+        "sheetId": 0,
+        "dimension": "COLUMNS",
+        "length": image.width
+      }
+    },
+    {
+        "updateDimensionProperties": {
+            "range": {
+                "sheetId": 0,
+                "dimension": "COLUMNS",
+                "startIndex": 3,
+                "endIndex": image.width + 4
+            },
+            "properties": {
+                "pixelSize": 25
+            },
+            "fields": "pixelSize"
+        }
+    },
+    {
+        "updateDimensionProperties": {
+            "range": {
+                "sheetId": 0,
+                "dimension": "ROWS",
+                "startIndex": 0,
+                "endIndex": max(image.height, len(questions)) + 1
+            },
+            "properties": {
+                "pixelSize": 25
+            },
+            "fields": "pixelSize"
+        }
+    }]
+    for y in range(image.height):
+        for x in range(image.width):
+            qidx = randrange(len(questions))
+            r,g,b = colors[y * image.height + x]
+            requests.append({
+                'addConditionalFormatRule': {
+                    'rule': {
+                    'ranges': [{
+                        'sheetId': 0,
+                        'startRowIndex': y,
+                        'endRowIndex': y + 1,
+                        'startColumnIndex': x + 3,
+                        'endColumnIndex': x + 4,
+                    }],
+                    'booleanRule': {
+                        'condition': {
+                            'type': 'CUSTOM_FORMULA',
+                            'values': [{
+                                'userEnteredValue':
+                                    f'=AND({coordsToA1(2, qidx+2)}<>"",{coordsToA1(2, qidx+2)}={answers[qidx]})',
+                            }]
+                        },
+                        'format': {
+                            'backgroundColor': {
+                                'red': r / 255.0,
+                                'green': g / 255.0,
+                                'blue': b / 255.0,
+                            }
+                        }
+                    }
+                },
+                'index': 0
+                }
+            })
+
+    body = {
+        'requests': requests
+    }
+    response = service.spreadsheets() \
+        .batchUpdate(spreadsheetId=spreadsheet['spreadsheetId'], body=body).execute()
+    print('{0} cells updated.'.format(len(response.get('replies'))))
---
+++ server.py
@@ -0,0 +1,83 @@
+from save_excel import makeExcel
+from save_google import google as saveGoogle
+from flask import Flask, redirect, render_template, request, send_file, session, url_for
+from flask_session import Session
+from PIL import Image
+import os
+
+app = Flask(__name__)
+SESSION_TYPE = "filesystem"
+PERMANENT_SESSION_LIFETIME = 800
+
+app.config.update(SECRET_KEY=os.urandom(24))
+app.config.from_object(__name__)
+Session(app)
+
+@app.post('/excel')
+def sendExcel():
+    image = Image.open(request.files.get('photo')).convert('RGB')
+    questions = [q.strip() for q in request.form.get('questions').split('\n')]
+    answers = [a.strip() for a in request.form.get('answers').split('\n')]
+    excelfile = makeExcel(image, questions, answers)
+    return send_file(excelfile, download_name='worksheet.xlsx', as_attachment=True)
+
+from google_auth_oauthlib.flow import Flow
+from google.oauth2 import id_token
+from google.auth.transport import requests
+
+@app.get('/googleLogin')
+def googleLogin():
+    flow = Flow.from_client_secrets_file(
+        'client_secret.json',
+        ['https://www.googleapis.com/auth/spreadsheets']
+    )
+    flow.redirect_uri = url_for('googleCallback')
+    authorization_url, state = flow.authorization_url(
+        access_type='offline',
+        include_granted_scopes='true')
+    return redirect(authorization_url)
+
+@app.get('/googleCallback')
+def googleCallback():
+    flow = Flow.from_client_secrets_file(
+        'client_secret.json',
+        ['https://www.googleapis.com/auth/spreadsheets']
+    )
+    flow.redirect_uri = url_for('googleCallback')
+    flow.fetch_token(authorization_response=request.url)
+    session['credentials'] = {
+        'token': flow.credentials.token,
+        'refresh_token': flow.credentials.refresh_token,
+        'token_uri': flow.credentials.token_uri,
+        'client_id': flow.credentials.client_id,
+        'client_secret': flow.credentials.client_secret,
+        'scopes': flow.credentials.scopes,
+        'expiry': flow.credentials.expiry,
+    }
+    return redirect('/')
+
+@app.post('/google')
+def google():
+    try:
+        creds = session['credentials'].copy()
+        del creds['expiry']
+    except KeyError:
+        return 'no creds'
+    image = Image.open(request.files.get('photo')).convert('RGB')
+    questions = [q.strip() for q in request.form.get('questions').split('\n')]
+    answers = [a.strip() for a in request.form.get('answers').split('\n')]
+    saveGoogle(creds, 'Mogus', image, questions, answers)
+    return 'done :)'
+
+import datetime
+
+@app.get('/')
+def root():
+    has_google = False
+    try:
+        expiry = session['credentials']['expiry']
+        if expiry > datetime.datetime.now():
+            has_google = True
+    except Exception as e:
+        pass
+    return render_template('submit-image.html', has_google=has_google)
